ffdb8d76caa934b1685ebab293c6b0d53e810ed3
N0tstgr <vikashkasaudhan321@gmail.com>
2025-10-03 23:32:10 +0530

    pp

---
+++ 02_comments.sh
@@ -0,0 +1,34 @@
+
+D
+D
+B
+C
+C
+C
+C
+C
+C
+C
+#!/bin/bash
+echo "checking Comments"
+
+C
+C
+C
+C
+C
+C
+C
+C
+C
+C
+C
+C
+C
+C
+C
+C
+C
+this is multiline comment
+
+#this is a single line commen
